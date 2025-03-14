3af59eb6538cf38fb2d3d4005818a8b375e4fabe
JLi2007 <LiJames9532@gmail.com>
2025-03-14 16:46:06 -0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,116 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+// import Draggable from "react-draggable";
 
-export default function Home() {
+export default function Sandbox() {
+  const [breakpoints, setBreakpoints] = useState<
+    { id: number; type: string; width: number; height: number }[]
+  >([]);
+  const [count, setCount] = useState<number>(1);
+  const [sandboxCode, setSandboxCode] = useState<string>( `
+  <html>
+    <head>
+      <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    <body class="bg-gray-100 flex justify-center items-center h-screen">
+      <div class="p-6 max-w-sm bg-white shadow-lg rounded-lg">
+        <h1 class="text-2xl font-bold text-blue-500">Hello, Tailwind!</h1>
+        <button class="mt-4 px-4 py-2 bg-blue-500 text-white rounded" onClick={alert("clicked!")}>Click Me</button>
+      </div>
+    </body>
+  </html>
+`);
+
+  function addWidthBreakpoint() {
+    setBreakpoints([
+      ...breakpoints,
+      { id: count, type: "width", width: 0, height: 50 },
+    ]);
+    setCount(count + 1);
+  }
+
+  function addHeightBreakpoint() {
+    setBreakpoints([
+      ...breakpoints,
+      { id: count, type: "height", width: 0, height: 50 },
+    ]);
+    setCount(count + 1);
+  }
+
+  // function updateBreakpoint(id: number, type: string, value: string) {
+  //   console.log(id, type, value);
+  //   // draggable feature
+  // }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="relative overflow-hidden">
+      <div className="font-open w-screen h-screen max-w-screen max-h-screen bg-linear-to-b from-violet-200 via-fuchsia-100 to-white shadow-[10px_0_10px_-5px_rgba(0,0,0,0.5),-10px_0_10px_-5px_rgba(0,0,0,0.5)] backdrop-blur-lg">
+        <div className="flex flex-row">
+          <div className="w-1/3 h-screen">
+            <h1 className="p-5 font-semibold text-2xl text-transparent bg-clip-text bg-linear-to-r from-violet-700/20 via-fuchsia-800/60 to-fuchsia-800/60 animate-gradient-x">
+              Querify
+            </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="flex h-full w-full p-4 gap-4">
+              <div className="w-full p-4 rounded-lg shadow-lg">
+                <h2 className="text-xl font-bold mb-4">Code</h2>
+                {/* {breakpoints.map((bp) => (
+                <div key={bp.id} className="mb-2 p-2 border rounded">
+                  <label className="block font-semibold">
+                    {bp.type == "width" ? "Width:" : "Height:"}
+                  </label>
+                  <input
+                    type="number"
+                    value={bp.width}
+                    onChange={(e) =>
+                      updateBreakpoint(bp.id, "width", e.target.value)
+                    }
+                  />
+                </div>
+              ))} */}
+                <textarea
+                  className="editor h-[70%] w-full bg-white/20 rounded-lg p-3 text-sm overflow-auto resize-none"
+                  value={sandboxCode}
+                  onChange={(e) => {
+                    setSandboxCode(e.target.value);
+                  }}
+                  placeholder="enter code here"
+                ></textarea>
+
+                <div className="flex flex-col">
+                  <h4 className="text-lg font-bold mb-4">Breakpoints</h4>
+                  <button className="mt-4" onClick={addWidthBreakpoint}>
+                    + Width Breakpoint
+                  </button>
+                  <button className="mt-4" onClick={addHeightBreakpoint}>
+                    + Height Breakpoint
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center w-full mt-5">
+            <h2 className="text-xl font-bold mb-4">Live Preview</h2>
+            <div className="h-full w-full">
+              <iframe title="preview" className="w-full h-full rounded-lg" srcDoc={sandboxCode} ></iframe>
+            </div>
+
+            {/* {breakpoints.map((bp) => (
+            <Draggable
+              key={bp.id}
+              axis={bp.type === "height" ? "y" : "x"}
+              defaultPosition={{ x: 0, y: 0 }}
+            >
+              <iframe
+                title="preview"
+                style={{ width: "100%", height: "100%", border: "none" }}
+                srcDoc={`<style>@media (min-width: ${bp.width}px) { body { background: red; } }</style><h1>Preview</h1>`}
+              />
+            </Draggable>
+          ))} */}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
